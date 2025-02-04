a531fec5df706dd0ce15a85958316711fcf4849c
Ahmad Bustany <ahmadbustany06@gmail.com>
2025-02-04 06:04:46 +0200

    linq03

---
+++ Linq03Assignment/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using static Day_01_G01.ListGenerator;
+using static System.Net.Mime.MediaTypeNames;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region LINQ - Partitioning Operators
+            #region 1. Get the first 3 orders from customers in Washington
+            //var result = CustomerList.Where(c => c.City == "Washington").SelectMany(c=>c.Orders).Take(3);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region 2. Get all but the first 2 orders from customers in Washington.
+            //var result = CustomerList.Where(c => c.City == "Washington").SelectMany(c => c.Orders).Skip(2);
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region 3. Return elements starting from the beginning of the array until a number is hit that is less than its position in the array.
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var result = numbers.TakeWhile((v, i) => v >= i);
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region 4.Get the elements of the array starting from the first element divisible by 3.
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var result = numbers.SkipWhile(p => p % 3 != 0);
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine(item );
+            //}
+            #endregion
+            #region 5. Get the elements of the array starting from the first element less than its position.
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var result = numbers.SkipWhile((v, i) => v >= i);
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #endregion
+            #region LINQ - Quantifiers
+            #region 1. Determine if any of the words in dictionary_english.txt (Read dictionary_english.txt into Array of String First) contain the substring 'ei'.
+            //string[] arr = File.ReadAllLines("dictionary_english.txt");
+            //var result = arr.Where(a => a.Contains("ei"));
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region 2. Return a grouped a list of products only for categories that have at least one product that is out of stock.
+            //var result = ProductList
+            //    .GroupBy(p => p.Category)
+            //    .Where(p => p.Any(i => i.UnitsInStock == 0))
+            //    .Select(p => new { cat = p.Key, product = p.ToList() });
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine(item.cat);
+            //    Console.WriteLine("");
+            //    foreach(var r in item.product)
+            //    {
+            //        Console.WriteLine(r);
+            //    }
+            //    Console.WriteLine("");
+            //}
+            #endregion
+            #region 3. Return a grouped a list of products only for categories that have all of their products in stock.
+            //var result = ProductList.GroupBy(c => c.Category)
+            //    .Where(c => c.All(i => i.UnitsInStock > 0))
+            //    .Select(p => new { cat = p.Key, prods = p.ToList() });
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine(item.cat);
+            //    Console.WriteLine("");
+            //    foreach(var prod in item.prods)
+            //    {
+            //        Console.WriteLine(prod);
+            //    }
+            //}
+            #endregion
+            #endregion
+            #region LINQ – Grouping Operators
+            #region Use group by to partition a list of numbers by their remainder when divided by 5
+            //List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            //var result = numbers.GroupBy(n => n % 5);
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine($"Numbers with the remainder of {item.Key} when divided by 5:");
+            //    foreach(var n in item)
+            //    {
+            //        Console.WriteLine(n);
+            //    }
+            //    Console.WriteLine("");
+
+            //}
+            #endregion
+            #region Uses group by to partition a list of words by their first letter.Use dictionary_english.txt for Input
+            //string[] arr = File.ReadAllLines("dictionary_english.txt");
+            //var result = arr.GroupBy(n => n.First());
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine($"the words start with {item.Key}:");
+            //    foreach(var n in item)
+            //    {
+            //        Console.WriteLine(n);
+            //    }
+            //}
+            #endregion
+            #region Use Group By with a custom comparer that matches words that are consists of the same Characters Together
+            //string[] Arr = { "from", "salt", "earn", "last", "near", "form" };
+            //var result = Arr.GroupBy(p => new String(p.Trim().OrderBy(c => c).ToArray()));
+            //foreach(var item in result)
+            //{
+            //    foreach(var wod in item)
+            //    {
+            //        Console.WriteLine(wod);
+            //    }
+            //    Console.WriteLine("--------------");
+            //}
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ Linq03Demo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using static Day_01_G01.ListGenerator;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Quantifier Operators
+            #region Any
+            //Console.WriteLine(ProductList.Any(p=>p.UnitsInStock==0));
+            #endregion
+            #region All
+
+            #endregion
+            #region Contains
+
+            #endregion
+            #region sequence
+            //var seq1 = Enumerable.Range(0, 100);
+            //var seq2 = Enumerable.Range(0, 100);
+            //Console.WriteLine(seq1.SequenceEqual(seq2));
+            #endregion
+            #endregion
+            #region Transformation Operators
+            //List<string> Words = new List<string>() { "Ten", "Twenty", "Thirty", "Fourty" };
+            //int[] Numbers = { 10, 20, 30, 40 };
+            //var result = Numbers.Zip(Words);
+            //var result2 = Numbers.Zip(Words,(number,word)=>$"{number}={word}");
+            //var result3 = Numbers.Zip(Words, [1, 2, 3]);
+            //foreach(var item in result3)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Grouping Operators
+            #region Exapmle01
+            //var result = from P in ProductList
+            //             group P by P.Category;
+            //var result = ProductList.GroupBy(p => p.Category);
+            //foreach(var group in result)
+            //{
+            //    Console.WriteLine(group.Key);
+            //    Console.WriteLine(group.Count());
+            //    foreach(var item in group)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            #endregion
+            #region Example02
+            //var result = from P in ProductList
+            //             where P.UnitsInStock > 0
+            //             group P by P.Category
+            //             into Groups
+            //             where Groups.Count() < 10
+            //             select new { Category = Groups.Key, Count = Groups.Count() };
+            //var result = 
+            //foreach(var item in result)
+            //{
+            //    Console.WriteLine($"{item.Category}::{item.Count}");
+            //}
+            #endregion
+            #endregion
+            #region Partition Operators
+            //var result = ProductList.Where(p => p.UnitsInStock > 0).Take(5);
+            //var result2 = ProductList.Where(p => p.UnitsInStock > 0).TakeLast(5);
+            //var result3 = ProductList.Where(p => p.UnitsInStock > 0).SkipLast(5);
+            //var result4 = ProductList.Where(p => p.UnitsInStock > 0).Skip(5);
+            //int[] Numbers = { 3, 2, 3 };
+            //var result5 = Numbers.SkipWhile(number => number % 3 == 0);
+            //foreach(var item in result5)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Let & Into
+            //List<string> Names = new List<string>() { "Rana", "Ahmed", "Omar" };
+            //var result = from n in Names
+            //             select Regex.Replace(n, "[aeiouAIEOU]", string.Empty)
+            //             into New
+            //             where New.Length > 2
+            //             select New;
+            //var result = from n in Names
+            //            let N = Regex.Replace(n, "[aeiouAIEOU]", string.Empty)
+            //            where N.Length>2
+            //            select N;
+            //var result = Names.Select(N => Regex.Replace(N, "[aeiouAIEOU]", string.Empty)).Where(N=>N.Length>2);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+        }
+    }
+}
